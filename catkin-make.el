2f0691d761a31b012e4413a2a1164f845a96d7cc
Maverobot <quzhengrobot@gmail.com>
2019-04-18 18:24:00 +0200

    working version

--- catkin-make.el
+++ catkin-make.el
@@ -4,10 +4,19 @@
       (if (string= parent-dir "/") nil
         (catkin-make--recursively-up-find-file parent-dir target-file-name)))))
 
-(defun catkin-make-find-current-catkin-workspace ()
-  (interactive)
-  (message (catkin-make--recursively-up-find-file (spacemacs--file-path) ".catkin_workspace")))
+(defun catkin-make--find-current-catkin-workspace ()
+  (catkin-make--recursively-up-find-file (spacemacs--file-path) ".catkin_workspace"))
 
-(defun shell-command-test()
+(defun catkin-make-compile-current-workspace()
+  "Tidies the HTML content in the buffer using `tidy'"
   (interactive)
-  (async-shell-command "pwd"))
+  (let ((default-directory (catkin-make--find-current-catkin-workspace)))
+    (if default-directory
+        (async-shell-command
+         ;; command and parameters
+         "catkin_make -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release"
+         ;; output buffer
+         "*catkin_make Output*"
+         ;; name of the error buffer
+         "*catkin_make Error")
+      (message "The current file is apparently not under a ROS workspace. If it is, try to initialize the workspace with catkin_make first.") )))
