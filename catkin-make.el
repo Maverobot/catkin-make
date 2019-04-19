2bce1b86038d8055df76e5cbbf2b203094322bf3
Maverobot <quzhengrobot@gmail.com>
2019-04-19 22:19:35 +0200

    use exisint window-buffer if existed

--- catkin-make.el
+++ catkin-make.el
@@ -10,21 +10,26 @@
 (defun catkin-make--start-process ()
   (start-process
    "catkin_make"
-   "*catkin_make*"
+   catkin-make--output-buffer
    "catkin_make"
    "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release")
   )
 
+(defvar catkin-make--output-buffer "*catkin_make*")
+
 (defun catkin-make-compile-current-workspace()
-  "Tidies the HTML content in the buffer using `tidy'"
   (interactive)
   (let ((default-directory (catkin-make--find-current-catkin-workspace)))
     (if default-directory
         (progn
-          (split-window-below -20)
-          (other-window 1)
-          (switch-to-buffer (get-buffer-create "*catkin_make*"))
-          (erase-buffer)
+          (let* ((output-buffer-window (get-buffer-window catkin-make--output-buffer 'visible)))
+            (if output-buffer-window
+                ;; Select the output buffer window if it is already visible
+                (select-window output-buffer-window)
+              ;; Otherwise, create a new window below, select it and switch to the output buffer
+              (split-window-below -20)
+              (other-window 1)
+              (switch-to-buffer (get-buffer-create catkin-make--output-buffer))))
           (let ((process
                  (catkin-make--start-process)))
             (with-current-buffer (process-buffer process)
